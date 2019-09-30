9231381682c2724547b16414f49c696f2f7cce45
Nithin Mohan T K <nithinmohantk@hotmail.com>
2019-09-30 17:50:40 +0100

    initial code

---
+++ src/JobContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetcore3_cosmos
+{
+    public class JobContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseCosmos(
+                "https://localhost:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                "EFCoreDemo2"
+            );
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Job>().OwnsOne(j => j.Address);
+
+            modelBuilder.Entity<Job>().OwnsMany(j => j.Contacts);
+
+            modelBuilder.Entity<Job>().HasOne(j => j.AssignedResource);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dotnetcore3_cosmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the EFCore Cosmos DB Provider...");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var job = new Job
+                {
+                    Id = Guid.NewGuid(),
+                    Address = new Address
+                    {
+                        Line1 = $"{i} Some Street {i}",
+                        Line2 = $"Somewhere{i}",
+                        Town = "Birmingham",
+                        PostCode = "B90 {i}SS",
+                    },
+                    Contacts = new List<Contact>()
+                    {
+                        new Contact { Title = "Mr", FirstName = $"Craig {i}", LastName = "Mellon", TelephoneNumber = "34441234" },
+                        new Contact { Title = "Mrs", FirstName = $"Cara {i}", LastName = "Mellon", TelephoneNumber = "53665554" }
+                    },
+                    AssignedResource = new Resource
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Mr",
+                        FirstName = "Bob",
+                        LastName = "Builder",
+                        TelephoneNumber = "0800 1234567"
+                    }
+                };
+                using (var context = new JobContext())
+                {
+                    context.Database.EnsureCreated();
+                    context.Add(job);
+                    context.SaveChanges();
+                }
+            }
+
+            using (var context = new JobContext())
+            {
+                var job = context.Jobs.First();
+                // now load the resource and assign it to the Job
+                var resource1 = context.Resources.First(x => x.Id == job.AssignedResourceId);
+                job.AssignedResource = resource1;
+                Console.WriteLine($"Job created and retrieved with address: {job.Address.Line1}, {job.Address.PostCode}");
+                Console.WriteLine($"  Contacts ({job.Contacts.Count()})");
+                job.Contacts.ForEach(x =>
+                {
+                    Console.WriteLine($"    Name: {x.FirstName} {x.LastName}");
+                });
+                Console.WriteLine($"  Assigned Resource: {job.AssignedResource?.FirstName} {job.AssignedResource?.LastName}");
+            }
+
+
+
+            var resourceId = Guid.NewGuid();
+            var resource = new Resource
+            {
+                Id = resourceId,
+                Title = "Mr",
+                FirstName = "Bob",
+                LastName = "Builder",
+                TelephoneNumber = "0800 1234567"
+            };
+            var job1 = new Job
+            {
+                Id = Guid.NewGuid(),
+                Address = new Address
+                {
+                    Line1 = "Job 1 Address"
+                },
+                AssignedResource = resource
+            };
+            var job2 = new Job
+            {
+                Id = Guid.NewGuid(),
+                Address = new Address
+                {
+                    Line1 = "Job 2 Address"
+                },
+                AssignedResource = resource
+            };
+            using (var context = new JobContext())
+            {
+                context.Database.EnsureCreated();
+                context.Add(job1);
+                context.Add(job2);
+                context.SaveChanges();
+            }
+            using (var context = new JobContext())
+            {
+                var loadedResource = context.Resources.First(x => x.Id == resourceId);
+                // Load all jobs with the same assigned resource id
+                var jobs = context.Jobs.Where(x => x.AssignedResourceId == resourceId).ToList();
+                jobs.ForEach(job =>
+                {
+                    Console.WriteLine($"Job: {job.Id} - Resource: {job.AssignedResource?.FirstName} {job.AssignedResource?.FirstName}");
+                });
+            }
+        }
+    }
+}
